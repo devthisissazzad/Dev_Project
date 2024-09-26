c3ee3dc72b6163117969eb56b77397cb2f3954c2
Md Sazzad Hossain <106889461+dev-SazzadHossain@users.noreply.github.com>
2024-09-26 23:14:48 +0600

    add_navbar_home_section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Shop from "../Shop/Shop";
+import Navbar from "../../Components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <Shop />
     </div>
   );
