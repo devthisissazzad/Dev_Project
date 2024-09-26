68c011477b8a1b9c3d538b658211989cc34a4880
Amit Chackroborty <149465275+amitchackroborty04@users.noreply.github.com>
2024-09-26 18:24:21 +0600

    ShopItem_Added

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,12 +1,62 @@
 import React from "react";
+import Container from "../../Utils/Container";
+import ShopItem from "./ShopItem";
 
 const Shop = () => {
   return (
-    <div>
-      <h2>AMit Dev_branch Create</h2>
-      <h2>AMit Dev_branch Create 1234</h2>
-      <h2>AMit Dev_branch Create asdfasdfa</h2>
-    </div>
+    <section>
+      <Container>
+        <div className="flex gap-[57px] justify-center mb-[48px] ">
+          <h2 className="text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+            Salad
+          </h2>
+          <h2 className="text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+            Salad
+          </h2>
+          <h2 className="text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+            Salad
+          </h2>
+          <h2 className="text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+            Salad
+          </h2>
+        </div>
+        <div className="flex justify-between">
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+          </div>
+        </div>
+
+        <div className="flex justify-between mt-[24px]">
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+          </div>
+        </div>
+
+        <div className="flex justify-between mt-[24px]">
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+          </div>
+        </div>
+      </Container>
+    </section>
   );
 };
 
