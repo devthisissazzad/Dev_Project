0847a8c9302f33a054a560f8c8a4488926eba597
Amit Chackroborty <149465275+amitchackroborty04@users.noreply.github.com>
2024-09-27 19:49:34 +0600

    update

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -6,55 +6,71 @@ const Shop = () => {
   return (
     <section>
       <Container>
-        <div className="flex gap-[57px] justify-center mb-[48px] ">
-          <h2 className="text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+        <div className="flex gap-5  lg:gap-[57px] justify-center mb-6 lg:mb-[48px] ">
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
             Salad
           </h2>
-          <h2 className="text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
-          pizza
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+            pizza
           </h2>
-          <h2 className="text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
-          soups
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+            soups
           </h2>
-          <h2 className="text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
-          desserts
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+            desserts
           </h2>
         </div>
-        <div className="flex justify-between">
+        <div className="flex gap-3  lg:gap-6  flex-wrap gap-y-6">
           <div className="w-[48%] lg:w-[32%]">
-            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
           </div>
           <div className="w-[48%] lg:w-[32%]">
-            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
           </div>
           <div className="w-[48%] lg:w-[32%]">
-            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
           </div>
         </div>
-
-        <div className="flex justify-between mt-[24px]">
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
-          </div>
-        </div>
-
-        <div className="flex justify-between mt-[24px]">
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem title=" Caeser Salad" image="../public/images/shopimage.png" />
-          </div>
-        </div>
+      
       </Container>
     </section>
   );
--- src/Pages/Shop/ShopItem.jsx
+++ src/Pages/Shop/ShopItem.jsx
@@ -6,15 +6,15 @@ const ShopItem = ({title,image}) => {
       <h3 className="  text-red-300">
         <img className="w-full object-cover" src={image} alt="shopimage" />
 
-        <h3 className="text-[24px] font-semibold text-[#151515] mt-[32px] text-center ">
+        <h3 className="text-[18px] lg:text-[24px] font-semibold text-[#151515] mt-5 lg:mt-[32px] text-center ">
           {title}
         </h3>
-        <h4 className=" text-base font-normal text-[#737373]  mt-2 leading-[26px]">
+        <h4 className="text-[12px] lg:text-base font-normal text-[#737373]  mt-2 leading-[26px]">
           Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.
         </h4>
 
-        <div className="flex justify-center pb-10">
-          <button className="text-xl font-medium text-[#BB8506] uppercase px-[30px] py-5 border border-b-[3px] border-b-[#BB8506] rounded-[8px] bg-[#E8E8E8] hover:bg-[#111827] duration-300 mt-[24px] flex justify-center ">
+        <div className="flex justify-center pb-5  lg:pb-10">
+          <button className="text-[12px] lg:text-xl font-medium text-[#BB8506] uppercase px-4  lg:px-[30px] py-3  lg:py-5 border border-b-[3px] border-b-[#BB8506] rounded-[8px] bg-[#E8E8E8] hover:bg-[#111827] duration-300 mt-[24px] flex justify-center ">
             add to cart
           </button>
         </div>
