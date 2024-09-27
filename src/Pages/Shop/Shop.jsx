122155b3f7ba252b3c3c352f07b4c90cfacfdf71
aklima-kazal <aklimakajal11@gmail.com>
2024-09-27 23:28:14 +0600

    new-add

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Shop = () => {
   return (
     <div>
-      <h2>AMit Dev_branch Create</h2>
+      <h2 className="bg-slate-800">AMit Dev_branch Create</h2>
       <h2>sgdfsuaigu</h2>
     </div>
   );
