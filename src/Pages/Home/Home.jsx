ad30ecc070343e1befb8e7e170fbc360d31ba1df
Md Sazzad Hossain <106889461+dev-SazzadHossain@users.noreply.github.com>
2024-09-27 23:41:55 +0600

    change_home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import From_CallUs from "../FromOurMenu/From_CallUs";
-import Login from "../../components/Auth/Login/Login";
+
 const Home = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
