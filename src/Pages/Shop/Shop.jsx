fc1ff2a24e31ccb2f303d16e5ca04b71f5c0d30d
Amit Chackroborty <149465275+amitchackroborty04@users.noreply.github.com>
2024-09-27 23:09:13 +0600

    update

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -7,16 +7,16 @@ const Shop = () => {
     <section>
       <Container>
         <div className="flex gap-5  lg:gap-[57px] justify-center mb-6 lg:mb-[48px] ">
-          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase cursor-pointer ">
             Salad
           </h2>
-          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase cursor-pointer  ">
             pizza
           </h2>
-          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase cursor-pointer ">
             soups
           </h2>
-          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase ">
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase  cursor-pointer">
             desserts
           </h2>
         </div>
