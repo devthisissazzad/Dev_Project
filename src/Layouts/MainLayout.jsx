c56e3ce40895dd78baf54ae32d0dacdc4f550c17
aklima-kazal <aklimakajal11@gmail.com>
2024-09-28 00:01:11 +0600

    add-changes

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -4,13 +4,9 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     <div>
-      <nav>Header</nav>
-
       <div>
         <Outlet />
       </div>
-
-      <footer>Footer</footer>
     </div>
   );
 };
