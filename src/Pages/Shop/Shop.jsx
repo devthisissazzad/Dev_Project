3e2a1b85479688f0f23cc48f6c5a57570240583c
Amit Chackroborty <149465275+amitchackroborty04@users.noreply.github.com>
2024-09-27 23:50:21 +0600

    adden_shopItem

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,7 +1,79 @@
 import React from "react";
+import Container from "../../Utils/Container";
+import ShopItem from "./ShopItem";
 
 const Shop = () => {
-  return <div></div>;
+  return (
+    <section>
+      <Container>
+        <div className="flex gap-5  lg:gap-[57px] justify-center mb-6 lg:mb-[48px] ">
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase cursor-pointer ">
+            Salad
+          </h2>
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase cursor-pointer  ">
+            pizza
+          </h2>
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase cursor-pointer ">
+            soups
+          </h2>
+          <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase  cursor-pointer">
+            desserts
+          </h2>
+        </div>
+        <div className="flex gap-3  lg:gap-6  flex-wrap gap-y-6">
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+          <div className="w-[48%] lg:w-[32%]">
+            <ShopItem
+              title=" Caeser Salad"
+              image="../public/images/shopimage.png"
+            />
+          </div>
+        </div>
+      
+      </Container>
+    </section>
+  );
 };
 
 export default Shop;
