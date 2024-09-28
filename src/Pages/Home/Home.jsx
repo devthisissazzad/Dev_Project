e579b5ad65822e1083dcdd89bc0f239a9d55df81
tanvirtalukdersiam <149420877+tanvirtalukdersiam@users.noreply.github.com>
2024-09-28 16:03:53 +0600

    M_Banners Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import From_CallUs from "../FromOurMenu/From_CallUs";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div>Home Pages</div>;
 };
 
 export default Home;
---
+++ src/components/M_DESSERTS/M_Desserts.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Container from "../../Utils/Container/Container";
+import MenuBanner from "../../assets/MenuBanners/MenuBenners.png";
+import RightImage from "../../assets/HomeFromOurMenu/fromOurMenuRightImage.png";
+import { Link } from "react-router-dom";
+const M_Desserts = () => {
+  return (
+    <div>
+      <div className="  ">
+        <img
+          className=" relative h-[700px] w-full object-contain"
+          src={MenuBanner}
+          alt=""
+        />
+        <div className=" absolute inset-0">
+          <div>
+            <Container>
+              <div className="bg-dark2 h-[330px] w-[1096px]   opacity-70 mx-auto mt-44 rounded-[2px]">
+                <div className=" mx-auto w-[850px] py-[100px] ">
+                  <h4 className="text-center text-[45px] text-white font-semibold font-cinzel">
+                    DESSERTS
+                  </h4>
+                  <p className="w-[841px] text-center font-inter text-white font-medium mt-1">
+                    Lorem Ipsum has been the industry’s standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </div>
+            </Container>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default M_Desserts;
