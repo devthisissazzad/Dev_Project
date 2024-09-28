1175ffc7cbb27192c1fd13f12d121ae240ff40a2
laksmisanto <laksmisanto1998@gmail.com>
2024-09-28 23:35:23 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./Routes/Routes";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={routes} />
+    </>
+  );
 }
 
 export default App;
