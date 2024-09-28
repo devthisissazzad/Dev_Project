a767dc1fce4020664615cf41b07dd97a3756f35d
laksmisanto <laksmisanto1998@gmail.com>
2024-09-28 23:29:16 +0600

    update-app.jsx-

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
-import Loading from "./components/loading/Loading";
-import Error from "./pages/error/Error";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-const route = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route index element={<Error />} />
-      <Route path="/loading" element={<Loading />} />
-    </Route>
-  )
-);
-
 function App() {
-  return (
-    <>
-      <RouterProvider router={route} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
