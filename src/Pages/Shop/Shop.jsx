3d35b2942f419c56be4a2474a4426633fcea0158
Amit Chackroborty <149465275+amitchackroborty04@users.noreply.github.com>
2024-09-28 22:15:48 +0600

    npn _added_for_pagination

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import ShopItem from "./ShopItem";
 import Container from "../../Utils/Container/Container";
+import axios from "axios";
+import Pagination from "./Pagination";
 
 const Shop = () => {
+  let [cardData, setCartData] = useState([]);
+  useEffect(() => {
+    function cardfuntion() {
+      axios
+        .get("https://dummyjson.com/products")
+        .then((data) => {
+          console.log(data.data.products);
+          setCartData(data.data.products);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    cardfuntion();
+  }, []);
   return (
     <section>
       <Container>
+        {/* ===========shop nav======================= */}
         <div className="flex gap-5  lg:gap-[57px] justify-center mb-6 lg:mb-[48px] ">
           <h2 className="text-sm lg:text-[24px] font-medium text-[#151515] leading-[29.05px] uppercase cursor-pointer ">
             Salad
@@ -21,57 +39,11 @@ const Shop = () => {
             desserts
           </h2>
         </div>
-        <div className="flex gap-3  lg:gap-6  flex-wrap gap-y-6">
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem
-              title=" Caeser Salad"
-              image="../public/images/shopimage.png"
-            />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem
-              title=" Caeser Salad"
-              image="../public/images/shopimage.png"
-            />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem
-              title=" Caeser Salad"
-              image="../public/images/shopimage.png"
-            />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem
-              title=" Caeser Salad"
-              image="../public/images/shopimage.png"
-            />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem
-              title=" Caeser Salad"
-              image="../public/images/shopimage.png"
-            />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem
-              title=" Caeser Salad"
-              image="../public/images/shopimage.png"
-            />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem
-              title=" Caeser Salad"
-              image="../public/images/shopimage.png"
-            />
-          </div>
-          <div className="w-[48%] lg:w-[32%]">
-            <ShopItem
-              title=" Caeser Salad"
-              image="../public/images/shopimage.png"
-            />
-          </div>
-        </div>
+        {/* ==============================nav end========================== */}
       
+        
+        <Pagination itemsPerPage={9} cardData={cardData} />
+
       </Container>
     </section>
   );
