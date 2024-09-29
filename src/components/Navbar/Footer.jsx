abb3fbf602be78cffe058b8c4adf3dace8f1d81f
Imam Hossain <imamhosain95@gmail.com>
2024-09-29 12:34:02 +0600

    Basic responsive work done, need a little inspection

--- src/components/Navbar/Footer.jsx
+++ src/components/Navbar/Footer.jsx
@@ -2,26 +2,20 @@ import React from "react";
 import { FaFacebookF } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center">
+    <footer className="flex ">
       <div className="w-full text-white font-inter">
         {/* /// footer left and right side */}
-        <div className="flex flex-col lg:flex-row">
+        <div className="flex">
           {/* /// left side */}
-          <div
-            className=" bg-[#1F2937] flex flex-col items-center justify-center w-full gap-6 py-24 bg-blue_gray-900 pl-14 pr-[150px]
-          lg:py-8 lg:pr-8 md:p-5 sm:p-4
-          "
-          >
-            <a
-              className="mr-[30px] lg:text-[27px] md:mr-0 md:text-[26px] sm:text-[24px]"
-              href="#"
-            >
-              <h3 className="items-center">CONTACT US</h3>
-            </a>
-            <p className="text-center lg:text-[20px] text-[17px] font-medium leading-9 text-white-a700 ">
+          <div className=" bg-[#1F2937] py-24 items-center text-center w-full gap-6 bg-blue_gray-900">
+            <Link className="mb-[24px]  lg:text-[32px] md:mr-0 md:text-[26px] sm:text-[24px]">
+              CONTACT US
+            </Link>
+            <p className="text-center text-[17px] font-medium leading-9 text-white-a700 ">
               123 ABS Street, Uni 21, Bangladesh <br />
               +88 123456789 <br />
               Mon - Fri: 08:00 - 22:00 <br />
@@ -29,29 +23,32 @@ const Footer = () => {
             </p>
           </div>
           {/* /// right */}
-          <div className="flex flex-col items-start w-full py-24 bg-gray-900 gap-9 pl-[218px] pr-18">
-            <div className="flex flex-col items-center gap-[26px]">
-              <p className="text-[32px] font-medium text-white ">Follow US</p>
-              <p className="text-[20px] font-medium text-white">
+          <div className="flex flex-col w-full gap-0 bg-gray-900 ">
+            <div className="flex items-center flex-col md:items-center sm:items-center  lg:items-center gap-[20px]">
+              <h3 className=" mt-[97px] lg:text-[32px]  text-white ">
+                Follow US
+              </h3>
+              <p className="text-[20px] ml-[5px] mb-[10px] font-medium text-white">
                 Join us on social media
               </p>
             </div>
-            <div className="mb-10 ml-[65px] flex w-[20%] justify-between gap-5 lg:w-full md:ml-0 md:w-full">
-              <h3 className="h-[34px] w-[34px] text-white">
-                <FaFacebookF />
-              </h3>
-              <h3 className=" h-[34px] w-[34px] text-white">
-                <FaTwitter />
-              </h3>
-              <h3 className="h-[34px] w-[34px] text-white">
-                <FaInstagram />
-              </h3>
+            <div className="mt-[32px] flex pt-[0px] justify-center  gap-8">
+              <Link>
+                <FaFacebookF className="text-white w-[35px] h-[35px]" />
+              </Link>
+              <Link>
+                <FaTwitter className="text-white w-[35px] h-[35px]" />
+              </Link>
+              <Link>
+                <FaInstagram className="text-white w-[35px] h-[35px]" />
+              </Link>
             </div>
           </div>
         </div>
+
         {/* /// footer bottom part */}
         <div className="flex justify-center py-[10px] bg-black">
-          <div className="flex self-end justify-center px-2 container-xs md:px-3 lg:px-5">
+          <div className="flex self-end justify-center px-2 md:px-3 lg:px-5">
             <p className="text-[12px] font-medium text-white lg:text-[17px]">
               Copyright © CulinaryCloud. All rights reserved.
             </p>
