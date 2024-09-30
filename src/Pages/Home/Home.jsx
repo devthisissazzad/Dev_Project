fee285e72bc27cc7d617a51f8cda45a4579d6dc3
Md Sazzad Hossain <106889461+dev-SazzadHossain@users.noreply.github.com>
2024-10-01 00:32:25 +0600

    add_shope_HomeInside

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Shop from "../Shop/Shop";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Shop />
     </div>
   );
 };
